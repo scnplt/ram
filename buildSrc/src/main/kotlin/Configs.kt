2ef766547125b5d5a41d9a4b988a746f1033f659
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2022-10-09 21:03:25 +0300

    💄 Create lesson list item

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -9,9 +9,9 @@
 
 object Configs {
     const val APPLICATION_ID = "dev.sertan.android.ram"
-    const val COMPILE_SDK = 33
+    const val COMPILE_SDK = 32
     const val MIN_SDK = 21
-    const val TARGET_SDK = 33
+    const val TARGET_SDK = 32
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
