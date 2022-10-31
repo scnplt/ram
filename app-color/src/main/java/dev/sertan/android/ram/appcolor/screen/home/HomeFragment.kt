fb2095f1c2ce030e1264e11609ac7e0f815ae856
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2022-10-31 12:17:05 +0300

    🚧 Update the `HomeFragment` design and create its ViewModel

--- app-color/src/main/java/dev/sertan/android/ram/appcolor/screen/home/HomeFragment.kt
+++ app-color/src/main/java/dev/sertan/android/ram/appcolor/screen/home/HomeFragment.kt
@@ -12,26 +12,43 @@ package dev.sertan.android.ram.appcolor.screen.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import dev.sertan.android.ram.appcolor.R
 import dev.sertan.android.ram.appcolor.databinding.FragmentHomeBinding
+import dev.sertan.android.ram.appcolor.screen.home.HomeFragmentDirections.Companion.actionHomeFragmentToDrawingFragment
+import dev.sertan.android.ram.coreui.util.navigateTo
 import dev.sertan.android.ram.coreui.util.showToast
 import dev.sertan.android.ram.coreui.util.viewBinding
+import kotlinx.coroutines.flow.FlowCollector
 
 @AndroidEntryPoint
 internal class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding by viewBinding(FragmentHomeBinding::bind)
+    private val viewModel by viewModels<HomeViewModel>()
+
+    private val voiceSupportStateFlowCollector = FlowCollector<Boolean> { isActive ->
+        binding.changeVoiceSupportButton.isActivated = isActive
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpComponents()
+        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.voiceSupportState.collect(voiceSupportStateFlowCollector)
+            }
+        }
     }
 
     private fun setUpComponents(): Unit = with(binding) {
         helpButton.setOnClickListener { showToast("Not yet implemented") }
-        changeVoiceSupportButton.setOnClickListener { showToast("Not yet implemented") }
+        changeVoiceSupportButton.setOnClickListener { viewModel.changeVoiceSupportState() }
         trainingButton.setOnClickListener { showToast("Not yet implemented") }
         practiceButton.setOnClickListener { showToast("Not yet implemented") }
-        sendFeedbackTextView.setOnClickListener { showToast("Not yet implemented") }
+        drawingButton.setOnClickListener { navigateTo(actionHomeFragmentToDrawingFragment()) }
     }
 }
