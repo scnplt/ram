d662c5232ea81667fe75881fce6a83879e4e5984
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2022-11-28 15:24:11 +0300

    ♻️ Update `lint` configuration

--- core-ui/src/main/java/dev/sertan/android/ram/coreui/RamApplication.kt
+++ core-ui/src/main/java/dev/sertan/android/ram/coreui/RamApplication.kt
@@ -11,16 +11,23 @@ package dev.sertan.android.ram.coreui
 
 import android.app.Application
 import android.content.Context
+import androidx.hilt.work.HiltWorkerFactory
 import androidx.multidex.MultiDex
+import androidx.work.Configuration
 import timber.log.Timber
 
-abstract class RamApplication : Application() {
+abstract class RamApplication : Application(), Configuration.Provider {
+
+    abstract val workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         // TASK: Check internet connection and show a message if no connection
     }
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder().setWorkerFactory(workerFactory).build()
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
