feb11e4ce7760aef3d3bde278470b51b3244cddf
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2022-12-11 17:24:39 +0300

    🐛 Fix: Slow image loading

--- core/ui/src/main/kotlin/dev/sertan/android/ram/core/ui/util/RamGlideModule.kt
+++ core/ui/src/main/kotlin/dev/sertan/android/ram/core/ui/util/RamGlideModule.kt
@@ -15,32 +15,27 @@ import com.bumptech.glide.Priority
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
-import com.bumptech.glide.load.engine.cache.LruResourceCache
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.signature.ObjectKey
 import dev.sertan.android.ram.core.ui.R
 
-private const val MEM_CACHE_SIZE_BYTES = 1024 * 1024 * 10L
-private const val DISK_CACHE_SIZE_BYTES = 1024 * 1024 * 50L
-private const val TIMEOUT = 5 * 1000
-private const val ENCODE_QUALITY = 100
+private const val DISK_CACHE_SIZE_BYTES = 1024 * 1024 * 100L
+private const val TIMEOUT = 1000 * 5
+private const val SIZE_MULTIPLIER = 0.6f
 
 @GlideModule
 class RamGlideModule : AppGlideModule() {
 
     override fun applyOptions(context: Context, builder: GlideBuilder): Unit = builder.run {
-        setMemoryCache(LruResourceCache(MEM_CACHE_SIZE_BYTES))
         setDiskCache(InternalCacheDiskCacheFactory(context, DISK_CACHE_SIZE_BYTES))
         setDefaultRequestOptions(getRequestOptions())
     }
 
     private fun getRequestOptions(): RequestOptions = RequestOptions()
         .priority(Priority.HIGH)
+        .sizeMultiplier(SIZE_MULTIPLIER)
         .error(R.drawable.ic_warning)
         .placeholder(R.drawable.ic_refresh)
-        .encodeQuality(ENCODE_QUALITY)
-        .signature(ObjectKey(System.currentTimeMillis()))
         .diskCacheStrategy(DiskCacheStrategy.ALL)
         .timeout(TIMEOUT)
 }
