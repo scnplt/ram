c91bc948b35119683c895bca59c23d4bd717c733
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2023-01-22 14:52:50 +0300

    ♻️ Reformat code

--- core/ui/src/main/kotlin/dev/sertan/android/ram/core/ui/util/FragmentExtensions.kt
+++ core/ui/src/main/kotlin/dev/sertan/android/ram/core/ui/util/FragmentExtensions.kt
@@ -14,8 +14,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.NavDirections
-import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlinx.coroutines.CoroutineScope
@@ -25,10 +23,6 @@ import kotlinx.coroutines.launch
 fun <VB : ViewBinding> Fragment.viewBinding(
     viewBindingFactory: (View) -> VB
 ): ReadOnlyProperty<Fragment, VB> = FragmentViewBindingDelegate(viewBindingFactory)
-
-fun Fragment.navTo(navDirections: NavDirections): Unit = findNavController().navigate(navDirections)
-
-fun Fragment.popBackStack(): Boolean = findNavController().popBackStack()
 
 fun Fragment.repeatOnLifecycleStarted(block: suspend CoroutineScope.() -> Unit) {
     viewLifecycleOwner.lifecycleScope.launch { repeatOnLifecycle(Lifecycle.State.STARTED, block) }
