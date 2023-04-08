86b572ba527faac7a69258ec28d014bb2920d442
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2023-04-08 03:45:08 +0300

    🎉 Create a new module for `app-communication`

---
+++ app-communication/src/main/kotlin/dev/sertan/android/ram/appcommunication/ui/home/HomeFragment.kt
@@ -0,0 +1,59 @@
+/*
+ * RAM (c) by Sertan Canpolat
+ *
+ * RAM is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work.
+ * If not, see <http://creativecommons.org/licenses/by-nc/4.0/>.
+ */
+
+package dev.sertan.android.ram.appcommunication.ui.home
+
+import android.os.Bundle
+import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
+import dev.sertan.android.ram.appcommunication.R
+import dev.sertan.android.ram.appcommunication.databinding.FragmentHomeBinding
+import dev.sertan.android.ram.appcommunication.ui.home.HomeFragmentDirections.Companion.actionHomeFragmentToDrawingFragment
+import dev.sertan.android.ram.appcommunication.ui.home.HomeFragmentDirections.Companion.actionHomeFragmentToEyeDetectionFragment
+import dev.sertan.android.ram.appcommunication.ui.home.HomeFragmentDirections.Companion.actionHomeFragmentToObjectRecognitionFragment
+import dev.sertan.android.ram.appcommunication.ui.home.HomeFragmentDirections.Companion.actionHomeFragmentToPoseDetectionFragment
+import dev.sertan.android.ram.appcommunication.ui.home.HomeFragmentDirections.Companion.actionHomeFragmentToSoundRecognitionFragment
+import dev.sertan.android.ram.core.ui.fragment.texttospeechprovider.TextToSpeechProviderFragment
+import dev.sertan.android.ram.core.ui.util.labelWithoutPrefix
+import dev.sertan.android.ram.core.ui.util.navTo
+import dev.sertan.android.ram.core.ui.util.navigateToOssLicenses
+import dev.sertan.android.ram.core.ui.util.viewBinding
+
+@AndroidEntryPoint
+internal class HomeFragment : TextToSpeechProviderFragment(R.layout.fragment_home) {
+
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+
+    override fun onTextToSpeechStateChanged(isActive: Boolean) {
+        binding.changeVoiceSupportButton.isActivated = isActive
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            titleTextView.text = requireContext().labelWithoutPrefix
+            changeVoiceSupportButton.setOnClickListener { changeTextToSpeechState() }
+            aboutButton.setOnClickListener { navigateToOssLicenses() }
+            attentionPracticeButton.setOnClickListener {
+                navTo(actionHomeFragmentToEyeDetectionFragment())
+            }
+            objectRecognitionButton.setOnClickListener {
+                navTo(actionHomeFragmentToObjectRecognitionFragment())
+            }
+            movementsButton.setOnClickListener {
+                navTo(actionHomeFragmentToPoseDetectionFragment())
+            }
+            soundsButton.setOnClickListener {
+                navTo(actionHomeFragmentToSoundRecognitionFragment())
+            }
+            drawingButton.setOnClickListener { navTo(actionHomeFragmentToDrawingFragment()) }
+        }
+    }
+}
---
+++ app-communication/src/main/kotlin/dev/sertan/android/ram/appcommunication/ui/soundrecognition/SoundRecognitionFragment.kt
@@ -0,0 +1,14 @@
+/*
+ * RAM (c) by Sertan Canpolat
+ *
+ * RAM is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work.
+ * If not, see <http://creativecommons.org/licenses/by-nc/4.0/>.
+ */
+
+package dev.sertan.android.ram.appcommunication.ui.soundrecognition
+
+import androidx.fragment.app.Fragment
+
+internal class SoundRecognitionFragment : Fragment()
