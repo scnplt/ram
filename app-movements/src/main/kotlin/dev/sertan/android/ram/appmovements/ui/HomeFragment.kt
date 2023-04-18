c79334eaf680d823b3abc2216bdd5ee006ba3117
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2023-04-18 17:54:39 +0300

    ♻️ Update `app-movements` module according to core module changes

--- app-movements/src/main/kotlin/dev/sertan/android/ram/appmovements/ui/HomeFragment.kt
+++ app-movements/src/main/kotlin/dev/sertan/android/ram/appmovements/ui/HomeFragment.kt
@@ -9,55 +9,45 @@
 
 package dev.sertan.android.ram.appmovements.ui
 
-import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import dagger.hilt.android.AndroidEntryPoint
 import dev.sertan.android.ram.appmovements.R
-import dev.sertan.android.ram.appmovements.databinding.FragmentHomeBinding
 import dev.sertan.android.ram.appmovements.ui.HomeFragmentDirections.Companion.actionHomeFragmentToDrawingFragment
 import dev.sertan.android.ram.appmovements.ui.HomeFragmentDirections.Companion.actionHomeFragmentToPoseDetectionFragment
-import dev.sertan.android.ram.core.ui.fragment.texttospeechprovider.TextToSpeechProviderFragment
-import dev.sertan.android.ram.core.ui.util.doIfPermissionGranted
-import dev.sertan.android.ram.core.ui.util.labelWithoutPrefix
-import dev.sertan.android.ram.core.ui.util.navTo
-import dev.sertan.android.ram.core.ui.util.navigateToOssLicenses
-import dev.sertan.android.ram.core.ui.util.viewBinding
+import dev.sertan.android.ram.core.ui.util.extension.doIfPermissionGranted
+import dev.sertan.android.ram.core.ui.util.extension.labelWithoutPrefix
+import dev.sertan.android.ram.core.ui.util.extension.navTo
+import dev.sertan.android.ram.core.ui.util.extension.resultLauncher
+import dev.sertan.android.ram.core.ui.util.extension.showToast
+import dev.sertan.android.ram.feature.home.BaseHomeFragment
+import dev.sertan.android.ram.feature.home.adapter.HomeListItem
 
 @AndroidEntryPoint
-internal class HomeFragment : TextToSpeechProviderFragment(R.layout.fragment_home) {
+internal class HomeFragment : BaseHomeFragment() {
 
-    private val binding by viewBinding(FragmentHomeBinding::bind)
+    private val cameraResultLauncher = resultLauncher(
+        onGranted = { navTo(actionHomeFragmentToPoseDetectionFragment()) },
+        onDenied = { showToast(dev.sertan.android.ram.core.ui.R.string.camera_permission_denied) }
+    )
 
-    private val requestPermissionLauncher = registerForActivityResult(RequestPermission()) {
-        if (it) {
-            navTo(actionHomeFragmentToPoseDetectionFragment())
-            return@registerForActivityResult
-        }
-
-        // TASK: Refactor - Use custom dialog
-        Toast.makeText(requireContext(), "Camera permission denied!", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onTextToSpeechStateChanged(isActive: Boolean) {
-        binding.changeVoiceSupportButton.isActivated = isActive
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        with(binding) {
-            titleTextView.text = requireContext().labelWithoutPrefix
-            changeVoiceSupportButton.setOnClickListener { changeTextToSpeechState() }
-            aboutButton.setOnClickListener { navigateToOssLicenses() }
-            startButton.setOnClickListener {
-                doIfPermissionGranted(
-                    resultLauncher = requestPermissionLauncher,
-                    permission = android.Manifest.permission.CAMERA,
-                    block = { navTo(actionHomeFragmentToPoseDetectionFragment()) }
-                )
-            }
-            drawingButton.setOnClickListener { navTo(actionHomeFragmentToDrawingFragment()) }
-        }
-    }
+    override val items: List<HomeListItem>
+        get() = listOf(
+            HomeListItem.TitleItem(title = requireContext().labelWithoutPrefix),
+            HomeListItem.HeaderItem(iconResId = R.drawable.ic_splash),
+            HomeListItem.ButtonItem(
+                buttonTextResId = R.string.start,
+                buttonIconResId = dev.sertan.android.ram.core.ui.R.drawable.ic_play,
+                onClicked = {
+                    doIfPermissionGranted(
+                        resultLauncher = cameraResultLauncher,
+                        permission = android.Manifest.permission.CAMERA,
+                        block = { navTo(actionHomeFragmentToPoseDetectionFragment()) }
+                    )
+                }
+            ),
+            HomeListItem.ButtonItem(
+                buttonTextResId = dev.sertan.android.ram.core.ui.R.string.drawing,
+                buttonIconResId = dev.sertan.android.ram.core.ui.R.drawable.ic_brush,
+                onClicked = { navTo(actionHomeFragmentToDrawingFragment()) }
+            )
+        )
 }
